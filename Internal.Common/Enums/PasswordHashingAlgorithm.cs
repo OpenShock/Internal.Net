3129bc62f75336af15968e2a104a58fed473f6a9
HeavenVR <github@heavenvr.tech>
2026-06-28 22:35:51 +0200

    Delete PasswordHashingAlgorithm.cs

--- Internal.Common/Enums/PasswordHashingAlgorithm.cs
+++
@@ -1,9 +0,0 @@
-﻿// ReSharper disable InconsistentNaming
-namespace OpenShock.Internal.Common.Enums;
-
-public enum PasswordHashingAlgorithm
-{
-    Unknown = -1,
-    BCrypt = 0,
-    PBKDF2 = 1,
-};
